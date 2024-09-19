ed502e9c085ebee6a840f5dfc7cf6665de7a3516
andrea.casaluci <casaluci10@gmail.com>
2024-09-19 11:25:46 +0200

    feat: prototype and prototype-registry demo

---
+++ prototype-registry/src/main/java/org/andrea/PrototypeRegistryDemo.java
@@ -0,0 +1,31 @@
+package org.andrea;
+
+import org.andrea.cache.BundledShapeCache;
+import org.andrea.shapes.Shape;
+
+public class PrototypeRegistryDemo {
+    public static void main(String[] args) {
+        BundledShapeCache cache = new BundledShapeCache();
+
+        Shape shape1 = cache.get("Big green circle");
+        Shape shape2 = cache.get("Medium blue rectangle");
+        Shape shape3 = cache.get("Medium blue rectangle");
+
+        if (shape1 != shape2 && !shape1.equals(shape2)) {
+            System.out.println("Big green circle != Medium blue rectangle (yay!)");
+        } else {
+            System.out.println("Big green circle == Medium blue rectangle (booo!)");
+        }
+
+        if (shape2 != shape3) {
+            System.out.println("Medium blue rectangles are two different objects (yay!)");
+            if (shape2.equals(shape3)) {
+                System.out.println("And they are identical (yay!)");
+            } else {
+                System.out.println("But they are not identical (booo!)");
+            }
+        } else {
+            System.out.println("Medium blue rectangle objects are the same (booo!)");
+        }
+    }
+}
